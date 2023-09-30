8f1eb9b6d3ac5074adad49a176cae53df7c68a17
Pritam <pritammahata12@gmail.com>
2023-09-30 23:00:51 +0530

    fix prog1

--- prog1.java
+++ prog1.java
@@ -3,9 +3,12 @@
 //    Input seconds: 86399
 //    23:59:59
 //    DONE
+import java.util.Scanner;
 public class prog1 {
     public static void main(String[] args) {
-        int hh, mm, ss, time = 86399;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Input seconds: ");
+        int hh, mm, ss, time = scn.nextInt();
         hh = time / 3600;
         mm = (time - hh * 3600) / 60;
         ss = time - hh * 3600 - mm * 60;
